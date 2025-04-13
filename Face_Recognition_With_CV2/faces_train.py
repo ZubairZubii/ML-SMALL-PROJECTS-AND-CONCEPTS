ba75eb710e6bf49bdb7398b49aa0970ab4fa01d8
👤 Zubair Ali <148703653+ZubairZubii@users.noreply.github.com>
2025-04-13 16:05:54 +0500

    Add files via upload

---
+++ Face_Recognition_With_CV2/faces_train.py
@@ -0,0 +1,51 @@
+#pylint:disable=no-member
+
+import os
+import cv2 as cv
+import numpy as np
+
+people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling']
+DIR = r'C:\Users\Zubair\Documents\Javascript\ML\Face_Recognition_With_CV2\Faces\train'
+
+
+haar_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+features = []
+labels = []
+
+def create_train():
+    for person in people:
+        path = os.path.join(DIR, person)
+        label = people.index(person)
+
+        for img in os.listdir(path):
+            img_path = os.path.join(path,img)
+
+            img_array = cv.imread(img_path)
+            if img_array is None:
+                continue 
+                
+            gray = cv.cvtColor(img_array, cv.COLOR_BGR2GRAY)
+
+            faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2)
+
+            for (x,y,w,h) in faces_rect:
+                faces_roi = gray[y:y+h, x:x+w]
+                features.append(faces_roi)
+                labels.append(label)
+
+create_train()
+print('Training done ---------------')
+
+features = np.array(features, dtype='object')
+labels = np.array(labels)
+
+# Local Binary Patterns Histograms (LBPH
+face_recognizer = cv.face.LBPHFaceRecognizer_create(radius=1, neighbors=8)
+
+# Train the Recognizer on the features list and the labels list
+face_recognizer.train(features,labels)
+
+face_recognizer.save('face_trained.yml')
+np.save('features.npy', features)
+np.save('labels.npy', labels)
